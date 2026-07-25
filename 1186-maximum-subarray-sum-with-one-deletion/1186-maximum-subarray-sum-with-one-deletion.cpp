d6a1760b9acfa752d7eb92f6580292fd134f76e8
Sohag Abir <sohagabir112@gmail.com>
2026-07-25 16:35:59 +0600

    Time: 0 ms (100%), Space: 27.4 MB (19.38%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -2,26 +2,42 @@ class Solution {
 public:
     int maximumSum(vector<int>& arr) {
         int n = arr.size();
-
-        int noDelete = arr[0];
-        int oneDelete = INT_MIN;
-        int ans = arr[0];
+        int power = 0;
+        int nopower = arr[0];
+        int res = arr[0];
 
         for(int i=1;i<n;i++){
-            int prevNodelete =  noDelete;
-            int prevOnedelete = oneDelete;
+            int v1 = arr[i];
+            int v2 = nopower + arr[i];
+            int v3 = power + arr[i];
+            int v4 = nopower;
 
-            noDelete = max(prevNodelete+arr[i], arr[i]);
-            
-            if(prevOnedelete == INT_MIN){
-                oneDelete = prevNodelete;
-            }
-            else{
-                int v2 = prevOnedelete + arr[i];
-                oneDelete = max(v2, prevNodelete);
-            }
-            ans = max(ans, max(oneDelete, noDelete));
+            res = max(res,max(max(v1,v2), max(v3,v4)));
+            //res = max(max(v1,v2), max(v3,v4));
+            nopower = max(v1,v2);
+            power = max(v3,v4);
         }
-        return ans;
+        return res;
+
+        // int noDelete = arr[0];
+        // int oneDelete = INT_MIN;
+        // int ans = arr[0];
+
+        // for(int i=1;i<n;i++){
+        //     int prevNodelete =  noDelete;
+        //     int prevOnedelete = oneDelete;
+
+        //     noDelete = max(prevNodelete+arr[i], arr[i]);
+            
+        //     if(prevOnedelete == INT_MIN){
+        //         oneDelete = prevNodelete;
+        //     }
+        //     else{
+        //         int v2 = prevOnedelete + arr[i];
+        //         oneDelete = max(v2, prevNodelete);
+        //     }
+        //     ans = max(ans, max(oneDelete, noDelete));
+        // }
+        // return ans;
     }
 };
