a773d1a17ba942cc6a8f04c757cfe667148a9b73
Sohag Abir <sohagabir112@gmail.com>
2026-07-29 20:50:15 +0600

    Time: 50 ms (72.66%), Space: 18.8 MB (39.42%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,24 +3,38 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
+        int row = n-1;
+        int col = 0;
 
-        for(int i=0;i<n;i++){
-            int low =0;
-            int high = m-1;
-            while(low <= high){
-                int mid = (low+high)/2;
+        while(row>=0 && col<m){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                row--;
+            }
+            else{
+                col++;
+            }
+        }
 
-                if(matrix[i][mid] == target){
-                    return true;
-                }
-                else if(matrix[i][mid] < target){
-                    low = mid + 1;
-                }
-                else{
-                    high = mid - 1; 
-                }
-        }
-        }
+        // for(int i=0;i<n;i++){
+        //     int low =0;
+        //     int high = m-1;
+        //     while(low <= high){
+        //         int mid = (low+high)/2;
+
+        //         if(matrix[i][mid] == target){
+        //             return true;
+        //         }
+        //         else if(matrix[i][mid] < target){
+        //             low = mid + 1;
+        //         }
+        //         else{
+        //             high = mid - 1; 
+        //         }
+        //     }
+        // }
         
         return false;
     }
